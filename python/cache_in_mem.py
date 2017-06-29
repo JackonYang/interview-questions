b561d5488ea27b88191746439b4ac28dd2f3cfeb
Jackon Yang <i@jackon.me>
2017-06-29 19:30:24 +0800

    add notes on how to commit answers

--- python/cache_in_mem.py
+++ python/cache_in_mem.py
@@ -5,6 +5,23 @@
 把计算过的结果，缓存在 `_cache_db` 中.
 第二次调用时，读取缓存中的数据，无需重复执行 add 函数。
 
+--------
+
+如何提交代码
+
+1. 在自己的 GitHub 上创建新的仓库。
+2. copy 这个文件到自己的仓库。
+3. commit 这个原始文件。
+4. 开始 coding
+5. commit 最终代码，并 push 到 GitHub
+6. 仓库的 URL 发给 HR
+
+
+Tips:
+
+1. 可以重复 commit 多次，以最终的文件为准。
+2. 良好的 commit history 也是 coding 能力的体现。
+
 """
 import hashlib
 
